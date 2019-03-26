3918e0da237bd4424393c42a839220418765640f
frenchie048 <37281870+frenchie048@users.noreply.github.com>
2019-03-26 13:51:49 -0700

    added file tree

---
+++ src/Components/Landing/landing.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Landing! Animations! Videos! Cool Stuff!
+            </div>
+        )
+    }
+}
---
+++ src/Components/LoginRegister/loginregister.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class LoginRegister extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Login/Register! Auth0! Things!
+            </div>
+        )
+    }
+}
