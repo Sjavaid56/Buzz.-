c8652e429b1f0ff20fb7394a618d8139bbfd6fd1
Zach Hirschman <Zach389395@yahoo.com>
2019-03-26 15:38:17 -0700

    Added routes

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react"
+import {Switch, Route} from "react-router-dom"
+import Landing from "./Components/Landing/landing"
+import LoginRegister from "./Components/LoginRegister/loginregister"
+import Dashboard from "./Components/Dashboard/dashboard"
+import userProfile from "./Components/UserProfile/UserProfile"
+import Tutorial from "./Components/Tutorial/Tutorial"
+import Finish from "./Components/Finish/Finish"
+
+
+export default(
+    <Switch>
+        <Route exact path = "/" component = {Landing}/>
+        <Route path = "/loginRegister" component = {LoginRegister}/>
+        <Route path = "/dashboard" component = {Dashboard}/>
+        <Route path = "/profile" component = {userProfile}/>
+        <Route path = "/finish" component = {Finish}/>
+        <Route path = "/tutorial" component = {Tutorial}/>
+    </Switch>
+)
