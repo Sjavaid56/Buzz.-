d194ce2db0791643d99c893cb78c970f93cabf86
frenchie048 <37281870+frenchie048@users.noreply.github.com>
2019-03-27 15:04:52 -0700

    comments styling

--- src/Components/Room/CurrentRoom/Comments/Comments.js
+++ src/Components/Room/CurrentRoom/Comments/Comments.js
@@ -15,13 +15,13 @@ export default class Comment extends Component {
     //For upvote and downvote functionality, Get comment_id to change and make a db request
     //to add or subtract one. Also, emit that action to all users.
     //Have not instantiated socket yet so can't emit yet.
-    upvote = (comment_id) =>{
+    upvote = (comment_id) => {
         // Endpoints not made yet
         // axios.put(`/commentUpVote/${comment_id}`).then(newCount =>{
         //     console.log("Upvoted post")
         // })
     }
-    downvote = (comment_id) =>{
+    downvote = (comment_id) => {
         // Endpoints not made yet
         // axios.put(`/commentDownVote/${comment_id}`).then(newCount =>{
         //     console.log("Upvoted post")
@@ -30,26 +30,32 @@ export default class Comment extends Component {
 
     render() {
         return (
-            <div className = "comment-parent">
-                <div className = "comment-parent__header">
+            <div className="comment-parent">
+                <div className="comment-parent__header">
+
                     {/* Change all data to equal props from Posts */}
-                    <div className = "comment-parent__user-info">
-                    <img className = "comment-parent__profile-picture" src = {this.props.commenter_img}/> 
-                    <p className = "comment-parent__user-name">@{this.props.commenter_user_name}</p>
+                    <div className="comment-parent__user-info">
+                        <img className="comment-parent__profile-picture" src={this.props.commenter_img} />
+
+                        <p className="comment-parent__user-name">@{this.props.commenter_user_name}</p>
                     </div>
-                    <p>3.29.19</p>
-                    <p>3:05pm</p>
+
+                    <div className='comment-parent__timestamp'>
+                        <p className='comment-parent__timeInfo'>3.29.19</p>
+                        <p className='comment-parent__timeInfo'>3:05pm</p>
+                    </div>
+
                 </div>
 
-                <div className = "comment-parent__content">
-                {this.props.comment_content}
+                <div className="comment-parent__content">
+                    {this.props.comment_content}
                 </div>
 
-                <div className = "comment-parent__footer">
+                <div className="comment-parent__footer">
                     <p>{this.props.comment_upvotes}</p>
-                    <button className = "comment-parent__vote"><img src = {up}></img></button>
+                    <button className="comment-parent__vote"><img src={up}></img></button>
                     <p>{this.props.comment_downvotes}</p>
-                    <button className = "comment-parent__vote"><img src = {down}></img></button>
+                    <button className="comment-parent__vote"><img src={down}></img></button>
                 </div>
             </div>
         )
