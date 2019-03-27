b1c922018820357708dfc4428ac32b9060a55185
Zach Hirschman <Zach389395@yahoo.com>
2019-03-26 19:23:08 -0700

    Created server, connected to db and session

---
+++ server/index.js
@@ -0,0 +1,32 @@
+//Require Packages
+const express = require("express")
+const bodyParser = require("body-parser")
+const massive = require("massive")
+const session = require("express-session")
+require("dotenv").config()
+
+//Build server
+const app = express()
+const server = require("http").createServer(app)
+const io = require("socket.io")(server)
+
+//app provisions
+app.use(bodyParser.json())
+app.use( express.static( `${__dirname}/../build` ) );
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    saveUninitialized:false,
+    resave:false
+}))
+
+//Connect to Database
+massive(
+    process.env.CONNECTION_STRING
+).then(db => {
+    console.log("Connected to Database")
+    app.set("db",db)
+}).catch(error =>{console.log(error)})
+
+//Server listen
+const port = process.env.PORT
+server.listen(port, ()=> console.log(`Server listening on port ${port}`));
