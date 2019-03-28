dd7352b5a1b7053ab3625b366aae5852ae0dbdbb
frenchie048 <37281870+frenchie048@users.noreply.github.com>
2019-03-28 12:49:32 -0700

    deleted login/register component, revised routes and re-route following login/register in authController

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
 import { Switch, Route } from "react-router-dom"
 import Landing from "./Components/Landing/Landing"
-import LoginRegister from "./Components/LoginRegister/LoginRegister"
 import Dashboard from "./Components/Dashboard/Dashboard"
 import userProfile from "./Components/UserProfile/UserProfile"
 import Tutorial from "./Components/Tutorial/Tutorial"
@@ -13,7 +12,6 @@ import Post from "./Components/Room/CurrentRoom/Posts/Posts";
 export default (
     <Switch>
         <Route exact path="/" component={Landing} />
-        <Route path="/loginRegister" component={LoginRegister} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/profile" component={userProfile} />
         <Route path="/tutorial" component={Tutorial} />
