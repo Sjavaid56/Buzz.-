8e94b16e143700dffc893839c42c41b5bd1ea884
Zach Hirschman <Zach389395@yahoo.com>
2019-03-29 11:50:18 -0700

    Fixed conflicts?

--- src/Components/Room/AvailableRooms/AvailableRooms.js
+++ src/Components/Room/AvailableRooms/AvailableRooms.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 import cafe from './icons/icons8-cafe-filled-30.png';
 //change this to svg or something we can use our custom color on
 import './availablerooms.css';
-import axios from "axios"
 
 export default class AvailableRooms extends Component {
     constructor(props) {
@@ -24,21 +23,13 @@ export default class AvailableRooms extends Component {
 
     //When screen loads, get all the rooms (change styling based on radius to user's geolocation) within a radius - maybe a mile - of the user's location
     componentDidMount = () => {
-<<<<<<< HEAD
-        axios.get("/getRooms").then(rooms => {
-=======
         axios.get('/getRooms').then(rooms => {
->>>>>>> 7fe1e8882e3079e3c2938d99adb41c7b3ed64be7
             this.setState({
                 rooms: rooms.data
             })
         })
     }
 
-<<<<<<< HEAD
-    render() {
-        let mappedRooms = this.state.rooms.map((room, index) => {
-=======
     joinSingleRoom = (room_id) => {
         let body = {
             business_name: this.state.rooms.business_name,
@@ -56,7 +47,6 @@ export default class AvailableRooms extends Component {
         console.log(this.state.currentRoomData)
 
         let mappedRooms = this.state.rooms.map((room) => {
->>>>>>> 7fe1e8882e3079e3c2938d99adb41c7b3ed64be7
             return (<div className='available-rooms'>
                 <div className='rooms-header'>
                     <h1 className='rooms-header__title'>
@@ -64,11 +54,7 @@ export default class AvailableRooms extends Component {
                     </h1>
                     <h3 className='rooms-header__distance'>
                         800 ft
-<<<<<<< HEAD
-                </h3>
-=======
                     </h3>
->>>>>>> 7fe1e8882e3079e3c2938d99adb41c7b3ed64be7
                 </div>
 
                 <div className='rooms-footer'>
@@ -78,11 +64,7 @@ export default class AvailableRooms extends Component {
 
                         <img style={{ height: 22, width: 22 }}
                             src={buzzBee} className='room-user__bee' />
-<<<<<<< HEAD
-                        <p>{room.number_of_users}</p>
-=======
                         <p>10</p>
->>>>>>> 7fe1e8882e3079e3c2938d99adb41c7b3ed64be7
                     </div>
 
                     <div className='rooms-footer__button'>
@@ -104,11 +86,7 @@ export default class AvailableRooms extends Component {
                     <h1>buzz.</h1>
                 </div>
 
-<<<<<<< HEAD
-                <main className = "available-container__mappedRooms">
-=======
                 <main className='mapped-businesses'>
->>>>>>> 7fe1e8882e3079e3c2938d99adb41c7b3ed64be7
                     {mappedRooms}
                 </main>
 
