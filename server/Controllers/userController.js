e953b4559ca58e23b2cfea0721e7299656089c4f
Zach Hirschman <Zach389395@yahoo.com>
2019-04-01 14:43:17 -0700

    Added drinks to profile

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -3,5 +3,12 @@ module.exports = {
     logoutUser: (req, res) => {
         req.session.destroy()
         res.status(200).end()
+    },
+    getDrinksForUser:(req,res) =>{
+        const db = req.app.get("db")
+        const {id} = req.params
+        db.get_drinks_for_user(id).then(response =>{
+            res.status(200).json(response)
+        })
     }
 }
