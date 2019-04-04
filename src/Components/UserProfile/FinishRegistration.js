529ce5305027a63462d9e3bdf32bf611770ce945
sjavaid56 <shawnjavaid@gmail.com>
2019-04-04 09:06:09 -0700

    Stripe working

--- src/Components/UserProfile/FinishRegistration.js
+++ src/Components/UserProfile/FinishRegistration.js
@@ -12,7 +12,8 @@ class FinishRegistration extends Component {
         this.state = {
             user: [],
             username: "",
-            picture: ""
+            picture: "",
+            card: false 
 
         }
     }
@@ -27,21 +28,21 @@ class FinishRegistration extends Component {
         })
 
     }
-
-    // onToken = (token) => {
-    //     // console.log('onToken', token)
-    //     Axios.post('/api/stripe', {
-    //       method: 'POST',
-    //       headers: {
-    //         Accept: 'application/json',
-    //         'Content-Type': 'application/json',
-    //       },
-    //       body: token.id
-    //     }).then(json => {
-    //          alert(json.data.message)
-    //         console.log('json', json)
-    //       }).catch
-    //     }
+    onToken = (token) => {
+        console.log('onToken', token); 
+        Axios.post('/stripe', {
+          method: "POST",
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: token.id
+        }).then( ()=> {
+          this.setState({
+            Card: true
+          })
+        })
+      }
 
 
     handleUsername() {
@@ -61,30 +62,24 @@ class FinishRegistration extends Component {
         console.log(this.state.picture)
 
         return (
-
-
             <div className='FinishReg-parent'>
                 <div className='profile-header'> </div>
 
-
-
                 <h3 className="h3">You're almost there....</h3>
-                <img src='https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=2364333683593240&height=50&width=50&ext=1556863656&hash=AeTOzd1VGZJca_ec' className="profile-photo" />
+                <img src={this.state.picture} className="profile-photo" />
 
                 <p1 className="username-text"> Let's give you a username first!</p1>
-                <input type="text" id="text" placeholder="Username" name="text_name" class="mytext" onChange={e => this.handleChange(e)} />
+                <input placeholder="Username" className="mytext" onChange={e => this.handleChange(e)} />
                 <Link to="/dashboard">
                     <button class="button-submit" onClick={() => this.handleUsername()}>submit</button>
                 </Link>
 
-                {/* <h2 className="card-text"> Optional: Add a payment method!</h2> */}
-                {/* <h3> <StripeCheckout
-          token={this.onToken}
-           // PUBLISHABLE KEY
-          stripeKey="pk_test_4S8gCnMZqqRJyrGjvtfMBgoS"
-           />
-         </h3> */}
-
+                <h2 className="card-text"> Add a payment, dont worry you can always do it later.</h2>
+                
+                 <StripeCheckout className="Stripe"
+                  token={this.onToken}
+                  stripeKey="pk_test_WaVHnhGJpp0M8KlijP56wMeL"
+                  />
             </div>
         )
     }
