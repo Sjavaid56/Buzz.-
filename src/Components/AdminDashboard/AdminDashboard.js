85b04a9a2419a7a21ef9d8694a5a4471c1b08876
Zach Hirschman <Zach389395@yahoo.com>
2019-04-06 15:18:00 -0700

    Added base styling for Admin Dashboard

--- src/Components/AdminDashboard/AdminDashboard.js
+++ src/Components/AdminDashboard/AdminDashboard.js
@@ -1,27 +1,35 @@
 import React, {Component} from "react"
 import Axios from "axios";
+import "./AdminDashboard.css"
+import AdminNavBar from "../AdminNavBar/AdminNavBar"
+import AdminMain from "../AdminMain/AdminMain"
 
 export default class AdminDashbaord extends Component{
     constructor(){
         super()
         this.state ={
-            adminPostData:[]
+            adminPostData:[],
+            adminData:[]
         }
     }
     componentDidMount = () =>{
-        setTimeout(() => {
             Axios.get("/getAdminPosts").then(allAdminPosts =>{
                 console.log("Got admin posts: ", allAdminPosts.data)
                 this.setState({
                     adminPostData:allAdminPosts.data
                 })
             })
-        }, 2000);
+            Axios.get("/getUserSession").then(adminData =>{
+                this.setState({
+                    adminData:adminData.data
+                })
+            })
     }
     render(){
         return(
-            <div>
-                ADMIN DASHBOARD
+            <div className = "Admin-Dash-Parent">
+                <AdminNavBar/>
+                <AdminMain/>
             </div>
         )
     }
