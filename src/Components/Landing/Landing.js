8b7a18ef76cc73e5608c3ebd1d4f4a8aea2c2945
frenchie048 <37281870+frenchie048@users.noreply.github.com>
2019-04-11 15:12:13 -0700

    all screen captures in assets and more styling

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 // import { Link } from "react-router-dom"
 import BuzzBeeCharcoal from '../../images/assets/logo/buzz-logo-charcoal-nobg.png';
 import BuzzBeeYellow from '../../images/assets/logo/buzz-logo-yellow-nobg.png'
-import screengrab from '../../images/screengrab-iphone1.png';
-import screengrab2 from '../../images/screengrab-iphone-2.png';
+// import screengrab from '../../images/iPhone/screengrab-iphone1.png';
+import screengrab2 from '../../images/iPhone/screengrab-iphone-2.png';
 import placeholderImg from '../../images/Portrait_Placeholder.png';
 import placeholderVideo from '../../images/Puppy-4740.mp4';
+import liLogo from '../../images/icons8-linkedin-104.png';
+import ghLogo from '../../images/icons8-github-filled-100.png';
 import './landing.css';
 import axios from 'axios';
 
@@ -45,7 +47,7 @@ export default class Landing extends Component {
                             <h1>buzz.</h1>
                             <img src={BuzzBeeYellow} alt='bee logo in yellow' />
                         </div>
-                        <p style={{ padding: '12px 0' }}>Pariatur cupidatat laboris ea fugiat incididunt aliqua esse id duis enim quis. Pariatur cupidatat laboris ea fugiat incididunt aliqua esse id duis enim quis.</p>
+                        <p style={{ padding: '35px 0' }}>Pariatur cupidatat laboris ea fugiat incididunt aliqua esse id duis enim quis. Pariatur cupidatat laboris ea fugiat incididunt aliqua esse id duis enim quis.</p>
                         <button onClick={this.login}>Login/Register</button>
                     </div>
                 </div>
@@ -69,16 +71,40 @@ export default class Landing extends Component {
                             <img src={placeholderImg} />
                             <h2>Brittany French</h2>
                             <p>Ad eu esse eiusmod officia quis eiusmod consectetur ipsum sunt irure labore cillum cupidatat.</p>
+                            <div className='landing-bottom__logos'>
+                                <a href='www.linkedin.com/in/brittanyfrench' >
+                                    <img src={liLogo} alt='linkedin logo in black' />
+                                </a>
+                                <a href='github.com/frenchie048' >
+                                    <img src={ghLogo} alt='github logo in black' />
+                                </a>
+                            </div>
                         </div>
                         <div className='landing-bottom__aboutSection'>
                             <img src={placeholderImg} />
                             <h2>Zach Hirschman</h2>
                             <p>Ad eu esse eiusmod officia quis eiusmod consectetur ipsum sunt irure labore cillum cupidatat.</p>
+                            <div className='landing-bottom__logos'>
+                                <a href='https://www.linkedin.com/in/zach-hirschman/' >
+                                    <img src={liLogo} alt='linkedin logo in black' />
+                                </a>
+                                <a href='https://github.com/zachhirschman' >
+                                    <img src={ghLogo} alt='github logo in black' />
+                                </a>
+                            </div>
                         </div>
                         <div className='landing-bottom__aboutSection'>
                             <img src={placeholderImg} />
                             <h2>Shawn Javaid</h2>
                             <p>Ad eu esse eiusmod officia quis eiusmod consectetur ipsum sunt irure labore cillum cupidatat.</p>
+                            <div className='landing-bottom__logos'>
+                                <a href='https://www.linkedin.com/in/shaian-javaid/' >
+                                    <img src={liLogo} alt='linkedin logo in black' />
+                                </a>
+                                <a href='https://github.com/Sjavaid56'>
+                                    <img src={ghLogo} alt='github logo in black' />
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
