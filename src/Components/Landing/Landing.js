c9b33a5fb19f5f3f84f89d9f4433229cc18679c0
frenchie048 <37281870+frenchie048@users.noreply.github.com>
2019-04-11 14:06:05 -0700

    styling for landing page - additional images is assets as well

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 // import { Link } from "react-router-dom"
 import BuzzBeeCharcoal from '../../images/assets/logo/buzz-logo-charcoal-nobg.png';
+import BuzzBeeYellow from '../../images/assets/logo/buzz-logo-yellow-nobg.png'
+import screengrab from '../../images/screengrab-iphone1.png';
+import screengrab2 from '../../images/screengrab-iphone-2.png';
+import placeholderImg from '../../images/Portrait_Placeholder.png';
+import placeholderVideo from '../../images/Puppy-4740.mp4';
 import './landing.css';
 import axios from 'axios';
 
@@ -32,11 +37,51 @@ export default class Landing extends Component {
     render() {
         return (
             <div className='landing-container'>
-                {/* <div className='landing-content'> */}
-                <img src={BuzzBeeCharcoal} alt='bee logo in charcoal' />
-                <h1>buzz.</h1>
-                <button onClick={this.login}>Login/Register</button>
-                {/* </div> */}
+
+                <div className='landing-top'>
+                    <img src={screengrab2} className='screenshotBuzz' />
+                    <div className='landing-top__right'>
+                        <div className='landing-top__rightLogo'>
+                            <h1>buzz.</h1>
+                            <img src={BuzzBeeYellow} alt='bee logo in yellow' />
+                        </div>
+                        <p style={{ padding: '12px 0' }}>Pariatur cupidatat laboris ea fugiat incididunt aliqua esse id duis enim quis. Pariatur cupidatat laboris ea fugiat incididunt aliqua esse id duis enim quis.</p>
+                        <button onClick={this.login}>Login/Register</button>
+                    </div>
+                </div>
+
+                <div className='landing-middle'>
+                    <div className='landing-middle__left'>
+                        <video autoPlay loop muted playsInline>
+                            <source src={placeholderVideo} type='video/mp4' />
+                        </video>
+                    </div>
+                    <div className='landing-middle__right'>
+                        <h1>What is buzz?</h1>
+                        <p>Pariatur cupidatat laboris ea fugiat incididunt aliqua esse id duis enim quis. Pariatur cupidatat laboris ea fugiat incididunt aliqua esse id duis enim quis. Pariatur cupidatat laboris ea fugiat incididunt aliqua esse id duis enim quis.</p>
+                    </div>
+                </div>
+
+                <div className='landing-bottom'>
+                    <h1>About The Founders</h1>
+                    <div className='landing-bottom__about'>
+                        <div className='landing-bottom__aboutSection'>
+                            <img src={placeholderImg} />
+                            <h2>Brittany French</h2>
+                            <p>Ad eu esse eiusmod officia quis eiusmod consectetur ipsum sunt irure labore cillum cupidatat.</p>
+                        </div>
+                        <div className='landing-bottom__aboutSection'>
+                            <img src={placeholderImg} />
+                            <h2>Zach Hirschman</h2>
+                            <p>Ad eu esse eiusmod officia quis eiusmod consectetur ipsum sunt irure labore cillum cupidatat.</p>
+                        </div>
+                        <div className='landing-bottom__aboutSection'>
+                            <img src={placeholderImg} />
+                            <h2>Shawn Javaid</h2>
+                            <p>Ad eu esse eiusmod officia quis eiusmod consectetur ipsum sunt irure labore cillum cupidatat.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
